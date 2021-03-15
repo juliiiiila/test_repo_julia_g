2325ea474ab2fb64dd9a03a926a19eaa8b28fcae
juliiiiila <79868886+juliiiiila@users.noreply.github.com>
2021-03-15 22:57:06 +0300

    Update base_pages.py

--- pages/django_pages/base_pages.py
+++ pages/django_pages/base_pages.py
@@ -1,7 +1,7 @@
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
-        self.base_page = 'http://172.22.0.4:8000/'
+        self.base_page = 'https://vk.com/feed'
 
     def open_base_page(self):
         self.driver.get(self.base_page)
