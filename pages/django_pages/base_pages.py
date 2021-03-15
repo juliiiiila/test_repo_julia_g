02f4415eb8cf372bb5d4a413d6c909ab7671e58e
dmitrysadovskii <48434081+dmitrysadovskii@users.noreply.github.com>
2021-03-15 16:57:06 +0300

    Update base_pages.py

--- pages/django_pages/base_pages.py
+++ pages/django_pages/base_pages.py
@@ -1,7 +1,7 @@
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
-        self.base_page = 'http://localhost:8000/'
+        self.base_page = 'http://172.22.0.4:8000/'
 
     def open_base_page(self):
         self.driver.get(self.base_page)
